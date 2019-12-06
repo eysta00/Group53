33a8a822a7bf1530b51f49fc4b9e56a7a75c849a
eysta00_ <eysi@live.com>
2019-12-06 00:42:10 +0000

    worked on employee ui

--- EmployeeUI.py
+++ EmployeeUI.py
@@ -5,18 +5,23 @@ class EmployeeUI:
         self.EmployeeLL = EmployeeLL()
 
     def register_employee(self):
+        print(10*"X")
         e_name = input("Full employee name: ")
         e_ssn = input("Employee social securtiy number: ")
         e_address = input("Employee Adress: ")
         e_phone = input("Employe phone number: ")
         e_email = input("Employee email: ")
-        e_pilot = input("Is employee a pilot? ")
+        e_pilot = bool(True)#input("Is employee a pilot? ")
         e_planelicense = input("Employee plane license: ")
-        error = self.EmployeeLL(e_name, e_ssn, e_address, e_phone, e_email, e_pilot, e_planelicense)
+        error = self.EmployeeLL.RegisterEmployee(e_name, e_ssn, e_address, e_phone, e_email, e_pilot, e_planelicense)
+        #print(error)
         if error != 1:
             print("Error, input not valid!")
         
         return
+    
+    def list_employees(self):
+        
 
-test1 = EmployeeUI()
-test1.register_employee()
+#test1 = EmployeeUI()
+#test1.register_employee()
