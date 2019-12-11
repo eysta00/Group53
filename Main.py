7182ea91edd72445751b782f35ac914287157a0f
ForgottenShade <gf00642@gmail.com>
2019-12-11 10:45:14 +0000

    Changed main to locate MenuUI in folder

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # This file will run the program as a whole
 # Main -> UI files -> Logic Layers -> Data layers (IO)
 #            ^              <-             <-
-from MenuUI import MenuUI
+from UI.MenuUI import MenuUI
 main = MenuUI()
 main.showMainMenu()
