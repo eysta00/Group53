a892cf514ba474daa8af573e9b7af78b3bec8342
eysta00 <eysi@live.com>
2019-12-11 11:39:51 +0000

    changed employee ui to import the API and made a main_eysta.py

--- UI/EmployeeUI.py
+++ UI/EmployeeUI.py
@@ -1,8 +1,9 @@
-from LogicLayer.EmployeeLL import EmployeeLL
+from LogicLayer.LLAPI import LLAPI
 
 class EmployeeUI:
     def __init__(self):
         self.EmployeeLL = EmployeeLL()
+        self.LLAPI = LLAPI()
 
     def register_employee(self):
         print("Register a new employee")
@@ -18,7 +19,7 @@ class EmployeeUI:
         else:
             e_pilot = False
         e_planelicense = input("Employee plane license: ")
-        error = self.EmployeeLL.RegisterEmployee(e_name, e_ssn, e_address, e_phone, e_email, e_pilot, e_planelicense)
+        error = self.LLAPI.RegisterEmployee(e_name, e_ssn, e_address, e_phone, e_email, e_pilot, e_planelicense)
         #print(error)
         if error != 1:
             print("Error, input not valid!")
@@ -27,37 +28,41 @@ class EmployeeUI:
         return
     
     def print_all_employees(self):
-        print("List all employess")
-        all_employees = self.EmployeeLL.ListAllEmployees()
+        print("\tList all employess")
+        all_employees = self.LLAPI.ListAllEmployees()
         for employee in all_employees:
             print(employee)
         print("\n")
         
     def print_flight_attendants(self):
-        print("List all flight attendats")
-        flight_attendants = self.EmployeeLL.ListFlightAttendats()
+        print("\tList all flight attendats")
+        flight_attendants = self.LLAPI.ListFlightAttendants()
         for attendant in flight_attendants:
             print(attendant)
         print("\n")
     
     def print_pilots(self):
         print("List all pilots")
-        all_pilots = self.EmployeeLL.ListPilots()
+        all_pilots = self.LLAPI.ListPilots()
         for pilot in all_pilots:
             print(pilot)
         print("\n")
     
+    def print_unassigned_employees():
+        print("\tList all unassigned employees")
+
+
     def print_pilots_with_aircraft_privilage(self):
-        print("List all pilots with a certain aircraft privilage")
+        print("\tList all pilots with a certain aircraft privilage")
         aircraft_model = input("Input aircraft model (case sensitive): ")
-        pilot_licenses = self.EmployeeLL.ListPilotsWithAircraftPrivilege(aircraft_model)
+        pilot_licenses = self.LLAPI.ListPilotsWithAircraftPrivilege(aircraft_model)
         for pilot_license in pilot_licenses:
             print(pilot_license)
         print("\n")
         
-test1 = EmployeeUI()
-test1.print_all_employees()
-#test1.register_employee()
+#test1 = EmployeeUI()
+#test1.print_all_employees()
+# test1.register_employee()
 # test1.print_flight_attendants()
 # test1.print_pilots()
 # test1.print_pilots_with_aircraft_privilage()
---
+++ main_eysta.py
@@ -0,0 +1,10 @@
+from UI.EmployeeUI import EmployeeUI
+X = "a"
+while X != "q":
+    test1 = EmployeeUI()
+    test1.print_all_employees()
+    # test1.register_employee()
+    test1.print_flight_attendants()
+    test1.print_pilots()
+    test1.print_pilots_with_aircraft_privilage()
+    X = input("type q to quit, anything else to continue")
